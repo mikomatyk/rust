6372837a4e422928910ddc1d3dbdf292903f7e67
Mikołaj Bartnicki <github@nikolaos.org>
2025-01-14 00:41:16 +0100

    Implement random number generation

--- RustBook/guessing_game/src/main.rs
+++ RustBook/guessing_game/src/main.rs
@@ -1,9 +1,15 @@
+// Rust Book
+// 2. Programming a Guessing Game
+
+use rand::Rng;
 use std::io;
 
 fn main() {
 	println!("Guess the number!");
-	println!("Please input your guess.");
+	let secret_number = rand::thread_rng().gen_range(1..=100);
+	println!("The secret number is: {secret_number}");
 
+	println!("Please input your guess.");
 	let mut guess = String::new();
 
 	io::stdin()
